5ccb90c77c49760339091efe27a15030b70a6899
Albinus Soren <albinussor@gmail.com>
2026-09-22 10:26:56 +0530

    Track coin mission progress

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -3,6 +3,7 @@ public class Coin : MonoBehaviour
 {
  public int value=5;
  void Update(){transform.Rotate(0,180f*Time.deltaTime,0); transform.position += Vector3.up*Mathf.Sin(Time.time*4f+transform.position.z)*.0015f;}
- void OnTriggerEnter(Collider other){if(other.GetComponent<PlayerController>()!=null){other.GetComponent<PlayerController>().CollectCoin(value);Destroy(gameObject);}}
- void OnCollisionEnter(Collision c){var p=c.collider.GetComponent<PlayerController>();if(p!=null){p.CollectCoin(value);Destroy(gameObject);}}
+ void Collect(PlayerController p){p.CollectCoin(value); if(SaveSystem.Data.missions!=null)SaveSystem.Data.missions.totalCoins+=Mathf.Max(0,value); SaveSystem.Save(); Destroy(gameObject);}
+ void OnTriggerEnter(Collider other){var p=other.GetComponent<PlayerController>();if(p!=null)Collect(p);}
+ void OnCollisionEnter(Collision c){var p=c.collider.GetComponent<PlayerController>();if(p!=null)Collect(p);}
 }

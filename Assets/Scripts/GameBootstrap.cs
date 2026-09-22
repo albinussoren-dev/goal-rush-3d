39632a4b9438907e4473643573768b08c6880613
Albinus Soren <albinussor@gmail.com>
2026-09-22 10:21:46 +0530

    Add bootstrap and runtime initialization

---
+++ Assets/Scripts/GameBootstrap.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class GameBootstrap:MonoBehaviour {
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+    static void AutoBoot(){if(FindFirstObjectByType<GameBootstrap>()==null)new GameObject("GameBootstrap").AddComponent<GameBootstrap>();}
+    void Awake(){if(GameManager.I!=null)return;new GameObject("GameManager").AddComponent<GameManager>();new GameObject("TrackManager").AddComponent<TrackManager>();new GameObject("UIManager").AddComponent<UIManager>();new GameObject("AudioManager").AddComponent<AudioManager>();var p=new GameObject("Player");p.AddComponent<CharacterController>();p.AddComponent<PlayerController>();var cam=new GameObject("Main Camera");cam.AddComponent<Camera>();var cc=cam.AddComponent<CameraController>();cc.target=p.transform;cam.tag="MainCamera";DontDestroyOnLoad(cam);}
+    void Start(){GameManager.I.SetState(GameState.MainMenu);}
+}
